71991c80dd21af5c656f2ffb1d87a94ae7cc2402
邱依良 <553579048@qq.com>
2022-10-09 23:29:25 +0800

    fix Circular Dependency for rabbitmq in Order module

--- emall-order/src/main/java/com/nju/emall/order/config/MyRabbitConfig.java
+++ emall-order/src/main/java/com/nju/emall/order/config/MyRabbitConfig.java
@@ -1,6 +1,7 @@
 package com.nju.emall.order.config;
 
 import org.springframework.amqp.core.Message;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.connection.CorrelationData;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
@@ -8,6 +9,7 @@ import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import javax.annotation.PostConstruct;
 
@@ -19,8 +21,17 @@ import javax.annotation.PostConstruct;
 @Configuration
 public class MyRabbitConfig {
 
-    @Autowired
-    RabbitTemplate rabbitTemplate;
+    private RabbitTemplate rabbitTemplate;
+
+    @Primary
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        this.rabbitTemplate = rabbitTemplate;
+        rabbitTemplate.setMessageConverter(messageConverter());
+        initRabbitTemplate();
+        return rabbitTemplate;
+    }
 
     @Bean
     public MessageConverter messageConverter() {
@@ -29,19 +40,21 @@ public class MyRabbitConfig {
 
     /**
      * 定制RabbitTemplate
-     * MyRabbitConfig对象创建完成之后 执行这个方法
+     * 1、服务收到消息就会回调
+     *      1、spring.rabbitmq.publisher-confirms: true
+     *      2、设置确认回调
+     * 2、消息正确抵达队列就会进行回调
+     *      1、spring.rabbitmq.publisher-returns: true
+     *         spring.rabbitmq.template.mandatory: true
+     *      2、设置确认回调ReturnCallback
      *
-     * 消费端确认(保证每个消息都被正确消费，此时才可以broker删除这个消息)
-     *  1、默认自动确认，只要消息接收到，客户端自动返回ACK确认，服务器就会移除这个消息，
-     *      但是有可能在收到消息之后就宕机了 ， 没来得及处理，消息就丢失了
-     *  2、手动确认，只要没有明确告诉MQ被接收，没有ACK，消息就一直都是Unacked
-     *      即使Consumer宕机，消息不会丢失，会重新变为Ready，下一次新的Consumer链接进来就发给它
+     * 3、消费端确认(保证每个消息都被正确消费，此时才可以broker删除这个消息)
      *
      */
-    @PostConstruct
+    // @PostConstruct  //MyRabbitConfig对象创建完成以后，执行这个方法
     public void initRabbitTemplate() {
-        // 设置自定义的确认回调
-         /**
+
+        /**
          * 1、只要消息抵达Broker就ack=true
          * correlationData：当前消息的唯一关联数据(这个是消息的唯一id)
          * ack：消息是否成功收到
@@ -49,14 +62,10 @@ public class MyRabbitConfig {
          */
         //设置确认回调
         rabbitTemplate.setConfirmCallback((correlationData,ack,cause) -> {
-            /**
-             * 1.做好消息确认机制  手动ACK
-             * 2.每一个发送的消息都在数据库做好记录。定期将失败的消息再次发送
-             */
-            // 服务器收到了
             System.out.println("confirm...correlationData["+correlationData+"]==>ack:["+ack+"]==>cause:["+cause+"]");
         });
-        // 只要消息没有投递到队列。就会出发这个失败回调
+
+
         /**
          * 只要消息没有投递给指定的队列，就触发这个失败回调
          * message：投递失败的消息详细信息
@@ -66,7 +75,6 @@ public class MyRabbitConfig {
          * routingKey：当时这个消息用哪个路邮键
          */
         rabbitTemplate.setReturnCallback((message,replyCode,replyText,exchange,routingKey) -> {
-            // 报错误了 ， 修改数据库当前消息的状态 把状态改为错误
             System.out.println("Fail Message["+message+"]==>replyCode["+replyCode+"]" +
                     "==>replyText["+replyText+"]==>exchange["+exchange+"]==>routingKey["+routingKey+"]");
         });
